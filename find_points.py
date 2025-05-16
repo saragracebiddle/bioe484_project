fix rotation by pairs

find_points.py
@@ -50,22 +50,34 @@ def compute_np_left(image, intensity_thresh=20):
     return 0, int(rows * 0.75)
 
 def get_np(img):
-    # https://blog.finxter.com/top-5-methods-to-find-and-draw-extreme-points-of-an-object-in-images-using-opencv-and-python/
-    #bottom_two_thirds = img[]
-    ret, thresh = cv.threshold(img, 5,255, cv.THRESH_BINARY)
+    h,w = img.shape
+    Y = h//2
+    yline = img[Y,:]
+    startX = np.min(yline.nonzero())
+    endX = np.max(yline.nonzero())
+    X = (endX - startX) //2 + startX
+    xline = img[:,X]
+    startY = np.min(xline.nonzero()) 
+    endY = np.max(xline.nonzero()) 
+    third = (endY - startY) // 3
+    top = int(endY)
+    bottom = int((endY - third))
+    bottomthird = img[bottom:top, :]
+    mins = []
+    for i in range(bottomthird.shape[0]):
+        mins.append(np.min(bottomthird[i,:].nonzero()))
 
-    cnts, _ = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    c = cnts[0]
+    minx = min(mins)
+    minxline = bottomthird[:,minx]
+    maxy = np.max(minxline.nonzero())
+  
 
-    extLeft = tuple(c[c[:,:,0].argmin()][0])
-
-
-    return extLeft
+    return (minx, bottom + maxy)
 
 def np_all(stack):
     output = np.zeros((stack.shape[0],2))
     for i, img in enumerate(stack):
-        cp = compute_np_left(img)
+        cp = get_np(img)
         output[i] = cp
 
     return output

rotations.py
@@ -24,22 +24,42 @@ def rotate_image(image, angle, image_center):
   result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
   return result
 
-def rotate_all(stack):
+def rotate_pair(stack):
     nps = np_all(stack)
     cps = cp_all(stack)
 
     avgtheta, allthetas  = avg_theta(cps, nps)
 
+    #print(avgtheta)
     output = stack.copy()
     for i, img in enumerate(stack):
-        rotate_by = int(avgtheta - allthetas[i])
 
-        if avgtheta > allthetas[i]:
-            rotate_by = -rotate_by
+        rotate_by = int(avgtheta - allthetas[i][0])
+        #print(allthetas[i][0])
+        #print(rotate_by)
+        #if avgtheta > allthetas[i]:
+        #    rotate_by = -rotate_by
+        
+        #print(rotate_by)
         output[i] = rotate_image(img, int(rotate_by), cps[i])
 
     return output
 
+
+def rotate_all(stack):
+    rotated = np.empty_like(stack)
+    numpairs = stack.shape[0]//2
+    for i in range(numpairs):
+        pair = np.zeros((2, stack.shape[1], stack.shape[2]))
+        pair[0] = stack[int(2*i)]
+        pair[1] = stack[int((2*i) + 1)]
+
+        r = rotate_pair(pair)
+
+        rotated[int(2*i)] = r[0,:,:]
+        rotated[int(2*i +1)] = r[1,:,:]
+
+    return rotated
 
 def rotate_image_about_cp_square(image, cp, angle_deg):
     h, w = image.shape